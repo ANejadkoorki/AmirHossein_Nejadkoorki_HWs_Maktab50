55b7181fc3750711f357f96dcd8bae70360bee09
Amir Hossein <anejadkoorki8199@gmail.com>
2021-03-08 23:56:34 +0330

    The answer to the second question was also added

--- HW6/AmirHossein_Nejadkoorki_HW6_Maktab50.py
+++ HW6/AmirHossein_Nejadkoorki_HW6_Maktab50.py
@@ -16,3 +16,55 @@ def markup_counter(type_of_products, count):   # type_of_products is string and 
 
     markup = (m * count) + y_intercept if 1 <= count < lower_count else lower_cost
     return markup
+
+
+
+def total_price_calculator(product_type, count,userid = None):
+    answer = dict()
+    markup = markup_counter(product_type,count)
+    for i in product_list:
+        if i["type_of_products"] == product_type:   # product name
+            answer['product_name'] = i["name"]
+        else:
+            continue
+        answer['total_price'] = ( count * i["price"] ) +  ((markup / 100) * ( count * i["price"] ) )   # total price
+
+        product_commision_groups = [q for q in i["commission_groups"] ]  #  commision
+        discount = 0
+        for j in discount_list:
+            if j["group_name"] in product_commision_groups and userid in j["users"] and j["unit"] == "percent":
+                discount += answer['total_price'] *  (  j["cost"] / 100 )
+                continue
+            if j["group_name"] in product_commision_groups and userid in j["users"] and j["unit"] == "Dollar":
+                discount += j["cost"]
+                continue
+            continue
+        answer['total_with_commission'] = answer['total_price'] - discount
+        answer['discount'] = discount
+        for z in user_list:   # user
+            if userid == None:
+                    answer['username'] = dict()
+                    answer['username']['first_name'] = ''
+                    answer['username']['last_name'] = ''
+                    break
+            elif z["userid"] == userid:
+                    answer['username'] = dict()
+                    answer['username']['first_name'] = z["first_name"]
+                    answer['username']['last_name'] = z["last_name"]
+                    break
+            else:
+                 continue
+        break
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
