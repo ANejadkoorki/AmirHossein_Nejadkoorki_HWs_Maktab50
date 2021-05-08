d5fc5226729fbf4b9d4c2fbf873ca10b27bd01e4
Amir Hossein <anejadkoorki8199@gmail.com>
2021-05-08 10:40:23 +0430

    update project HW8

--- HW8/HomeWork8/FirstApp/models.py
+++ HW8/HomeWork8/FirstApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class SlideShowPost(models.Model):
+    title = models.CharField(verbose_name='title of post', max_length=200)
+    caption = models.TextField(verbose_name='caption')
--- HW8/HomeWork8/FirstApp/views.py
+++ HW8/HomeWork8/FirstApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . import models
 
 # Create your views here.
 
@@ -7,6 +7,10 @@ def home_view(request):
     """
     it`s a view for home part of page!!!
     """
+    slide_show_posts = models.SlideShowPost.objects.all()
     return render(request,
-                  template_name="blog/home.html"
+                  context={
+                    "slide_show_posts" : slide_show_posts
+                  },
+                  template_name="FirstApp/home.html"
                   )
