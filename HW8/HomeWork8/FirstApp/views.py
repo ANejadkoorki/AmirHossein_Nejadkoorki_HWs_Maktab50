9847ca11ede049995fdd68dfc1fa16b5e53e1e34
Amir Hossein <anejadkoorki8199@gmail.com>
2021-05-12 19:59:50 +0430

    CONTACT US completion

--- HW8/HomeWork8/FirstApp/views.py
+++ HW8/HomeWork8/FirstApp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
-from . import models,forms
+from . import models, forms
 
 
 # Create your views here.
@@ -12,27 +12,17 @@ def home_view(request):
     slide_show_posts = models.SlideShowPost.objects.all()
     about_post = models.AboutPost.objects.all()
     services = models.Services.objects.all()
-    return render(request,
-                  context={
-                      "slide_show_posts": slide_show_posts,
-                      "about_post": about_post,
-                      "services_post": services,
-                  },
-                  template_name="FirstApp/home.html"
-                  )
-
-
-def contact_view(request):
-    """
-    saves information of visitor
-    """
     form_instance = forms.PostForm()
     if request.method == 'POST':
         form_instance = forms.PostForm(data=request.POST)
         if form_instance.is_valid():
             form_instance.save()
-    return render(
-        request,
-        'blog/Create_post.html',
-        {'post_form': form_instance}
-    )
+    return render(request,
+                  context={
+                      "slide_show_posts": slide_show_posts,
+                      "about_post": about_post,
+                      "services_post": services,
+                      "post_form": form_instance
+                  },
+                  template_name="FirstApp/home.html"
+                  )
